c01e4f485addfd950050f4b90f3d9d988f7b01bc
Raiko Kirn <Raiko.Kirn@team.telstra.com>
2023-02-12 19:29:30 +1100

    Task3_Final1

--- src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
+++ src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
@@ -3,6 +3,7 @@ package stepDefinitions;
 import au.com.telstra.simcardactivator.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
@@ -11,53 +12,42 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 
 import io.cucumber.java.en.Given;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = SimCardActivator.class, loader = SpringBootContextLoader.class)
 public class SimCardActivatorStepDefinitions {
     @Autowired
     private TestRestTemplate restTemplate;
+    private JsonPayload jsonPayload;
 
-    @Given("Valid SIM Card ID has been provided attempt activation")
-    public void validSIMCardIDHasBeenProvided() {
-        String validsim;
-        validsim = "1255789453849037777";
-        String activateurl;
-        activateurl = "http://localhost:8080/activate";
-        JsonPayload validsimObject = new JsonPayload();
-        validsimObject.setIccid(validsim);
-        var activate1 = restTemplate.postForObject(activateurl, validsimObject, Void.class);
+    @Given("a valid SIM Card ID has been provided")
+    public void aValidSIMCardIDHasBeenProvided() {
+        jsonPayload = new JsonPayload("1255789453849037777", "rkvalid@gurumail.com", false);
     }
 
-    @Then("Confirm result of Valid SIM Card activation")
-    public void attemptToActivateValidSIMCard() {
-        String validsimquery;
-        validsimquery = "http://localhost:8080/query?simCardId=1";
-        var queryEntry1 = restTemplate.getForObject(validsimquery, ResponseActuator.class);
-        System.out.println(queryEntry1);
+    @Given("an invalid SIM Card ID has been provided")
+    public void anInvalidSIMCardIDHasBeenProvided() {
+        jsonPayload = new JsonPayload("8944500102198304826", "rkinvalid@gurumail.com", false);
 
     }
 
-
-    @Given("Invalid SIM Card ID has been provided attempt activation")
-    public void invalidSIMCardIDHasBeenProvidedAttemptActivation() {
-        String invalidsim;
-        invalidsim = "8944500102198304826";
-        String activateurl;
-        activateurl = "http://localhost:8080/activate";
-        JsonPayload invalidsimObject = new JsonPayload();
-        invalidsimObject.setIccid(invalidsim);
-        var activate2 = restTemplate.postForObject(activateurl, invalidsimObject, Void.class);
-    }
-
-    @Then("Confirm result of Invalid SIM Card activation")
-    public void confirmResultOfInvalidSIMCardActivation() {
-        String invalidsimquery;
-        invalidsimquery = "http://localhost:8080/query?simCardId=2";
-        var queryEntry2 = restTemplate.getForObject(invalidsimquery, ResponseActuator.class);
-        System.out.println(queryEntry2);
+    @When("a request to activate SIM Card is submitted")
+    public void aRequestToActivateSIMCardIsSubmitted() {
+        this.restTemplate.postForObject("http://localhost:8080/activate", jsonPayload, String.class);
 
     }
 
+    @Then("confirm valid SIM Card activation attempt status from database")
+    public void confirmValidSIMCardActivationAttemptStatusFromDatabase() {
+        var jsonPayload = this.restTemplate.getForObject("http://localhost:8080/query?simCardId={simCardId}", JsonPayload.class, 1);
+        assertTrue(jsonPayload.getActive());
+    }
 
+    @Then("confirm invalid SIM Card activation attempt status from database")
+    public void confirmInvalidSIMCardActivationAttemptStatusFromDatabase() {
+        var jsonPayload = this.restTemplate.getForObject("http://localhost:8080/query?simCardId={simCardId}", JsonPayload.class, 2);
+        assertFalse(jsonPayload.getActive());
+    }
 }
--- src/test/resources/features/sim_card_activator.feature
+++ src/test/resources/features/sim_card_activator.feature
@@ -2,12 +2,14 @@ Feature: sim card activator
   This feature activates SIM Cards
 
   Scenario: Valid SIM Card Activation
-    Given Valid SIM Card ID has been provided attempt activation
-    Then Confirm result of Valid SIM Card activation
+    Given a valid SIM Card ID has been provided
+    When a request to activate SIM Card is submitted
+    Then confirm valid SIM Card activation attempt status from database
 
   Scenario: Invalid SIM Card Activation
-    Given Invalid SIM Card ID has been provided attempt activation
-    Then Confirm result of Invalid SIM Card activation
+    Given an invalid SIM Card ID has been provided
+    When a request to activate SIM Card is submitted
+    Then confirm invalid SIM Card activation attempt status from database
 
 
 
